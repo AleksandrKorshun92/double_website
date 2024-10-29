93d3754d0f476aff63bec920559a1d3c5dc48e1a
aleksander <spaik1192@mail.ru>
2024-10-29 06:41:55 +0300

    настройки secret key

--- shedule/settings.py
+++ shedule/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import LOGIN_REDIRECT_URL
@@ -23,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2_vm0+0dw7a&db&ha3ru@cn1=c3jgi-t&%=um(oq)v$$uje4cp'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
 CSRF_TRUSTED_ORIGINS = [
     'https://shedule-korshunovay.amvera.io',
     # Добавьте другие домены сюда, если необходимо
@@ -55,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
