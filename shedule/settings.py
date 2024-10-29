5f9e7af8fdd8619dc0288f68f954543163ef1590
aleksander <spaik1192@mail.ru>
2024-10-29 06:18:51 +0300

    настройки токена

--- shedule/settings.py
+++ shedule/settings.py
@@ -27,6 +27,13 @@ SECRET_KEY = 'django-insecure-2_vm0+0dw7a&db&ha3ru@cn1=c3jgi-t&%=um(oq)v$$uje4cp
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://shedule-korshunovay.amvera.io',
+    # Добавьте другие домены сюда, если необходимо
+]
+
 
 LOGIN_URL = '/login/'
 
