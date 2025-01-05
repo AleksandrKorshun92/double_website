99f3b6fe514045a5ed09ca0e62c6348fee6d0610
Aleksandr Korshunov <125506053+AleksandrKorshun92@users.noreply.github.com>
2025-01-05 10:55:11 +0300

    Update models.py

--- sheduleapp/models.py
+++ sheduleapp/models.py
@@ -1,8 +1,8 @@
+
 from django.db import models
 from django.contrib.auth.models import User
 
 class Judges(models.Model):
-    """Основная модель судей, с привязкой к определенному пользователю """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     arbitration_court = models.CharField(max_length=100)
     name = models.CharField(max_length=50)
@@ -15,7 +15,6 @@ class Judges(models.Model):
 
 
 class Site(models.Model):
-    """Основная модель сайтов, с привязкой к определенному пользователю """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.URLField()
     title = models.CharField(max_length=255)
@@ -24,8 +23,22 @@ class Site(models.Model):
         return self.title
 
 
+class Case(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=30, null=False, unique=True)
+    court = models.CharField(max_length=50)
+    costumer = models.CharField(max_length=40)
+    costumer_status = models.CharField(max_length=40)
+    other_costumer = models.CharField(max_length=40)
+    event = models.CharField(max_length=40)
+    event_date = models.DateTimeField(null=True, blank=True)
+    description_case = models.TextField(null=True, blank=True)
+    case_activ = models.CharField(max_length=20)
+    
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField(null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
+
 
